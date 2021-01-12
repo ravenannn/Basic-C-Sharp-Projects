e6bf51da4b514689892c903610086448ca2a3b03
Raven Sumner <ravenannn@gmail.com>
2021-01-12 16:41:33 -0700

    Daily Report Assignment
    Created a daily report for students at the Tech Academy in C#. Saved answers to questions in the program to the correct data type with clearly labeled variables.

---
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DailyReportAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
+            string positiveExperiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a greate day!");
+        }
+    }
+}
