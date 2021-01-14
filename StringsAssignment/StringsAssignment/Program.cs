03eaf2b27f8a93336acaecf343a6ee3cea7c6aa5
Raven Sumner <ravenannn@gmail.com>
2021-01-14 10:35:14 -0700

    Strings Assignment
    Create a console app that concatenates three strings, converts a string to uppercase, and creates a StringBuilder that builds a paragraph of text one sentence at a time.

---
+++ StringsAssignment/StringsAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+
+namespace StringsAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Concatenate three strings
+            string str1 = "Hi, my name";
+            string str2 = "is Raven, and";
+            string str3 = "I love the color purple.";
+            string concatted = str1 + " " + str2 + " " + str3;
+            Console.WriteLine(concatted);
+
+            // Convert string to uppercase
+            Console.WriteLine(concatted.ToUpper());
+
+            // Create Stringbuilder and build a paragraph of text one sentence at a time
+            StringBuilder sb = new StringBuilder("Hello, world!");
+            sb.Append(" What a beautiful day.");
+            sb.Append(" Would you like to go for a run?");
+            sb.Append(" It may snow again next week.");
+
+            Console.WriteLine(sb);
+
+
+            Console.ReadLine();
+        }
+    }
+}
