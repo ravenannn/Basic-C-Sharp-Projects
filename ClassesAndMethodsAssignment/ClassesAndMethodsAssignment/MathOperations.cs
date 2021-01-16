a2e98737af37d7e091a7ae5306b2c53d505c9d39
Raven Sumner <ravenannn@gmail.com>
2021-01-16 16:06:57 -0700

    Classes And Methods Assignment
    Create a class with three methods. Each method should take in an integet and execute a math operation on the received parameter. Put this class in a separate .cs file in the app.  Ask user in Main() program for a number and call each method in turn passing the user input to the method.

---
+++ ClassesAndMethodsAssignment/ClassesAndMethodsAssignment/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassesAndMethodsAssignment
+{
+    public class MathOperations
+    {
+       
+
+        public void Add12(int chosenNum)
+        {
+            int added = 12 + chosenNum;
+            Console.WriteLine(chosenNum + " plus 12 equals: " + added);
+        }
+
+        public void Mult3(int chosenNum)
+        {
+            int multiplied = 3 * chosenNum;
+            Console.WriteLine(chosenNum + " multiplied by 3 equals: " + multiplied);
+        }
+
+        public void Sub10(int chosenNum)
+        {
+            int subtracted = chosenNum - 10;
+            Console.WriteLine(chosenNum + " minus 10 equals: " + subtracted);
+        }
+    }
+}
---
+++ ClassesAndMethodsAssignment/ClassesAndMethodsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace ClassesAndMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number: ");
+            int chosenNum = Convert.ToInt32(Console.ReadLine());
+
+            MathOperations mathProblem = new MathOperations();
+
+            mathProblem.Add12(chosenNum);
+            mathProblem.Mult3(chosenNum);
+            mathProblem.Sub10(chosenNum);
+
+            Console.ReadLine();
+        }
+        
+
+    }
+}
