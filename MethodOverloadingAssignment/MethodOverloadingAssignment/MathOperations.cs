a1ec621473e78ac6e983f3f56255b00d246f55af
Raven Sumner <ravenannn@gmail.com>
2021-01-16 18:21:19 -0700

    Method Overloading
    Demonstrate Method Overloading passing an integer, a decimal, and a string value. Return all back as integers.

---
+++ MethodOverloadingAssignment/MethodOverloadingAssignment/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingAssignment
+{
+    public class MathOperations
+    {
+        public int mathOperations(int intValue)
+        {
+            int div2 = intValue / 2;
+            return div2;
+        }
+
+        public decimal mathOperations(decimal decValue)
+        {
+            int add10 = Convert.ToInt32(decValue) + 10;
+            return add10;
+        }
+
+        public int mathOperations(string strValue)
+        {
+            int mult5 = 5 * Convert.ToInt32(strValue);
+            return mult5;
+        }
+    }
+}
---
+++ MethodOverloadingAssignment/MethodOverloadingAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations divided = new MathOperations();
+            int intValue = 10;
+            int intValResult = divided.mathOperations(intValue);
+            Console.WriteLine(intValue + " divided by 2 equals: " + intValResult);
+            
+            MathOperations added = new MathOperations();
+            decimal decValue = 5.5m;
+            int decValResult = Convert.ToInt32(added.mathOperations(decValue));
+            Console.WriteLine(decValue + " plus 10 equals: " + decValResult);
+
+            MathOperations multiplied = new MathOperations();
+            string strValue = "3";
+            int strValResult = multiplied.mathOperations(strValue);
+            Console.WriteLine(strValue + " multiplied by 5 equals: " + strValResult);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
