26b62898e1cf939970579a2be08bdb61ddbf34ec
Raven Sumner <ravenannn@gmail.com>
2021-01-17 14:44:58 -0700

    Void Method Assignment
    Create a class with a void method that takes 2 integers as parameters. Perform math on the 1st integer and return 2nd integer by passing in two numbers and then by passing in parameters by name.

---
+++ VoidMethodAssignment/VoidMethodAssignment/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class MathOperations
+    { 
+        public void mathOperations(int intOne, int intTwo)
+        {
+            int intOneMath = intOne + 4;
+            Console.WriteLine(intTwo);
+        }
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations returnInt = new MathOperations();
+            int intOne = 3;
+            int intTwo = 5;
+
+            returnInt.mathOperations(intOne, intTwo);
+            returnInt.mathOperations(3, 5);
+            Console.ReadLine();
+
+        }
+    }
+}
