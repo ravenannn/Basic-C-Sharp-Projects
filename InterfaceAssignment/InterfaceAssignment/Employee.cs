68bf115d1920d18a9423280d7782f908967acd40
Raven Sumner <ravenannn@gmail.com>
2021-01-18 11:00:08 -0700

    Interface Assignment
    Create an interface with a void method. Have a class inherit the interface and implement its method. Use polymorphism to create an object of type IQuittable and call the method on it.

---
+++ InterfaceAssignment/InterfaceAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssignment
+{
+    public class Employee : IQuittable
+    {
+        public virtual void Quit()
+        {
+            Console.WriteLine("This employee has opted for voluntary termination.");
+        }
+    }
+}
---
+++ InterfaceAssignment/InterfaceAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use polymorphism to create an object of type IQuittable
+            IQuittable quit = new Employee();
+
+            // Call the Quit() method on it
+            quit.Quit();
+
+            Console.ReadLine();
+
+        }
+    }
+}
