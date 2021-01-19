b1cf6a2fc6ccd4125335ece85faa84bec70c0950
Raven Sumner <ravenannn@gmail.com>
2021-01-19 16:27:48 -0700

    DateTime Assignment
    Get current date and time. Ask user for a number. Print  to console the exact time it will be in X hours, X being the user input.

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Current date and time
+            var currentDateTime = DateTime.Now;
+            // Print current DateTime
+            Console.WriteLine("Current date and time is: " + currentDateTime);
+
+            // Ask user for a number
+            Console.WriteLine("Please choose a number: ");
+
+            // Convert and store user input as an integer
+            int chosenNum = Convert.ToInt32(Console.ReadLine());
+
+            // Print to console the time it will be in X hours, X being the user's input number
+            Console.WriteLine("The date and time in " + chosenNum + " hours will be: " + currentDateTime.AddHours(chosenNum));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
