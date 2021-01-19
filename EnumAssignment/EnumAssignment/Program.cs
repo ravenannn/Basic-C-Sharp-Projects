7069572ab93112fa04db7604966e5b51fa17ada1
Raven Sumner <ravenannn@gmail.com>
2021-01-18 17:01:56 -0700

    Enum Assignment
    Create an enum for days of the week. Take user input and assign the value to a variable of the enum data type. Wrap statement in a try/catch block and print error to console if a valid day is not input.

---
+++ EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        
+        // Create an enum
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            // Prompt user to enter the current day of the week
+            Console.WriteLine("Please enter the current day of the week:");
+            string input = Console.ReadLine();
+            var formattedInput = char.ToUpper(input[0]) + input.Substring(1);
+            var parsed = Enum.Parse(typeof(DaysOfTheWeek), formattedInput);
+            
+            Console.WriteLine("You chose " + input);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.WriteLine("The code successfully made it through this Try/Catch Block.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+    
+}
