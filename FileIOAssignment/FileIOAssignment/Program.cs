b02a1db9d4623533316b4cb030e06a3ac9741835
Raven Sumner <ravenannn@gmail.com>
2021-01-19 15:49:42 -0700

    File IO Assignment
    Create console app that asks user for a number. Log that number to a text file. Then print the text file back to the user.

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for a number
+            Console.WriteLine("Please choose a number: ");
+            // Save input
+            string chosenNum = Console.ReadLine();
+            // Append input to the log
+            using (StreamWriter file = new StreamWriter(@"C:\Users\raven\Logs\log2.txt", true))
+            {
+                file.WriteLine(chosenNum);
+            }
+            string text = File.ReadAllText(@"C:\Users\raven\Logs\log2.txt");
+            Console.WriteLine(text);
+
+            Console.ReadLine();
+        }
+
+    }
+}
