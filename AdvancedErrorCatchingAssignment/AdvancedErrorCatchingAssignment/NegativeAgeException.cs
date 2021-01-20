fbde89dfe4a95a11fbb4b626668607d6cd682da9
Raven Sumner <ravenannn@gmail.com>
2021-01-19 19:08:12 -0700

    Advanced Error Catching Assignment
    Create console app that asks user for their age. Display the year the user was born. Handle exceptions with try/catch. Display appropriate error message if user enters 0 or negative numbers. Display general message if there are other types of exceptions.

---
+++ AdvancedErrorCatchingAssignment/AdvancedErrorCatchingAssignment/NegativeAgeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedErrorCatchingAssignment
+{
+    class NegativeAgeException : Exception
+    {
+        // Create two constructors that will inherit from base exception
+        public NegativeAgeException()
+            //inheriting from the base exception
+            : base() { }
+        public NegativeAgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ AdvancedErrorCatchingAssignment/AdvancedErrorCatchingAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedErrorCatchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Ask user for their age
+                Console.WriteLine("How old will you turn this year?");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge <= 0)
+                {
+                    throw new NegativeAgeException();
+                }
+                // Get current DateTime
+                var currentDate = DateTime.Now;
+
+                // Subtract age in years from current date
+                var userBorn = currentDate.AddYears(-userAge);
+
+                //Display year user was born
+                Console.WriteLine("You were born in the year: " + userBorn.Year);
+
+            }
+            catch (NegativeAgeException)
+            {
+                Console.WriteLine("Please enter an age greater than zero.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error has occurred.");
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
