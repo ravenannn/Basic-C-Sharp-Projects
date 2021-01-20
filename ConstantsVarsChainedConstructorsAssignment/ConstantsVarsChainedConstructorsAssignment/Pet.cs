057c24492b20f5e6756458887c762156dae0f993
Raven Sumner <ravenannn@gmail.com>
2021-01-19 17:59:29 -0700

    Contstants, Variables, & Chained Constructors Assignment
    Create a constant variable. Create a variable using keyword "var". Chain two constructors together.

---
+++ ConstantsVarsChainedConstructorsAssignment/ConstantsVarsChainedConstructorsAssignment/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantsVarsChainedConstructorsAssignment
+{
+    class Pet
+    {
+        // Chained Constructor - automatically sets needsBath to true upon creation of pet 
+        public Pet(string name) : this(name, true)
+        {
+
+        }
+        public Pet(string name, bool needsBath)
+        {
+            Name = name;
+            NeedsBath = needsBath;
+        }
+
+       
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public bool NeedsBath { get; set; }
+    }
+}
---
+++ ConstantsVarsChainedConstructorsAssignment/ConstantsVarsChainedConstructorsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantsVarsChainedConstructorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a constant variable
+            const string shopName = "Pups N' Suds";
+            Console.WriteLine(shopName);
+
+            // Create a variable using keyword var
+            // This Pet also demonstrates the chained constructors
+            var pet = new Pet("Lucky");
+            Console.ReadLine();
+
+           
+        }
+    }
+}
