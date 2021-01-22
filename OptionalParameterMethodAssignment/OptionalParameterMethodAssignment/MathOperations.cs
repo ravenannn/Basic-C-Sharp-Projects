acfea2ed794c73de7008b43062108c7bb34a99b1
Raven Sumner <ravenannn@gmail.com>
2021-01-22 16:28:51 -0700

    Optional Parameter Assignment
    Create a class with a method that takes two integers as parameters that were obtained through user input. Make one optional. Have the method do a math operation and return an integer result.

---
+++ OptionalParameterMethodAssignment/OptionalParameterMethodAssignment/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethodAssignment
+{
+    class MathOperations
+    {
+        public int mathOperations(int num1, int optionalNum2)
+        {
+          
+            int optionalAdded = num1 + optionalNum2;
+            int addTen = 10 + optionalAdded;
+            return addTen;
+        }
+               
+    }
+}
+
---
+++ OptionalParameterMethodAssignment/OptionalParameterMethodAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations solveMath = new MathOperations();
+
+            Console.WriteLine("Please choose a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose a second number if you'd like: ");
+            string optionalNum2 = Console.ReadLine();
+            int convertedNum2 = 0;
+
+            if (optionalNum2.Length > 1)
+            {
+                convertedNum2 = Convert.ToInt32(optionalNum2);
+            }
+            
+            
+
+            int addedNums = solveMath.mathOperations(num1, convertedNum2);
+            Console.WriteLine("Your chosen number(s) added to ten equal: " + addedNums);
+            Console.ReadLine();
+        }
+    }
+}
