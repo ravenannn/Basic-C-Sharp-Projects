c4b26ebeccac767301ddc1f9050f09cdf594e442
Raven Sumner <ravenannn@gmail.com>
2021-01-22 14:00:51 -0700

    Entity Framework Code First Challenge
    Create a basic Entity Framwork Code-First app that creates a basic Student database and adds one student.

---
+++ EFCodeFirstAppChallenge/EFCodeFirstAppChallenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstAppChallenge
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var newStudent = new Student()
+                {
+                    firstName = "Sierra",
+                    lastName = "Miller",
+                    DateOfBirth = "11/12/2007",
+                    Height = 5.2m,
+                    Weight = 110
+                };
+
+                ctx.Students.Add(newStudent);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EFCodeFirstAppChallenge/EFCodeFirstAppChallenge/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstAppChallenge
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+    }
+}
